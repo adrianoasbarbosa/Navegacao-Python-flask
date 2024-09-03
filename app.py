4e9b76706b0bcecad0882c9e65ced7c3332d937c
adrianoasbarbosa <adrianodevsistemas@gmail.com>
2024-09-02 21:25:18 -0300

    Melhorando a NavBar e colocando fonte poppins, colocando rotas sobre e contato

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#Rota para a página sobre (Sobre)
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+#Rota para a página contato (Contato)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
